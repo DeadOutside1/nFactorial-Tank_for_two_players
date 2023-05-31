65389671c5dbe05e8b4d142cb60851a51a62dbb0
Amir <210103034@stu.sdu.edu.kz>
2023-06-01 04:01:47 +0600

    Add UI class, display hp for two tanks

--- Tanki/main.py
+++ Tanki/main.py
@@ -10,7 +10,28 @@ TILE = 50
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+fontUI = pygame.font.Font(None, 30)
+
 DIRECTS = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+
+
+class UI:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        i = 0
+        for obj in objects:
+            if obj.type == 'tank':
+                pygame.draw.rect(window, obj.color, (5 + i * 70, 5, 22, 22))
+
+                text = fontUI.render(str(obj.hp), 1, obj.color)
+                rect = text.get_rect(center=(5 + i * 70 + 32, 5 + 11))
+                window.blit(text, rect)
+                i += 1
 
 
 class Tank:
@@ -125,11 +146,12 @@ bullets = []
 objects = []
 Tank('blue', 100, 275, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE))
 Tank('red', 650, 275, 0, (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_KP_ENTER))
+ui = UI()
 
 for _ in range(50):
     while True:
         x = randint(0, WIDTH // TILE - 1) * TILE
-        y = randint(0, HEIGHT // TILE - 1) * TILE
+        y = randint(1, HEIGHT // TILE - 1) * TILE
         rect = pygame.Rect(x, y, TILE, TILE)
         fined = False
         for obj in objects:
@@ -149,10 +171,12 @@ while play:
 
     for bullet in bullets: bullet.update()
     for obj in objects: obj.update()
+    ui.update()
 
     window.fill('black')
     for bullet in bullets: bullet.draw()
     for obj in objects: obj.draw()
+    ui.draw()
 
     pygame.display.update()
     clock.tick(FPS)
